31c5a78d372b13c99b398bf317e3c6db6c421850
Joe Gadd <joe@computer.local>
2025-02-10 10:41:15 +0000

    Removed the dev stack for local development

--- cloud_infra/__main__.py
+++ cloud_infra/__main__.py
@@ -1,10 +1,10 @@
-"""An AWS Python Pulumi program"""
+"""AWS Infrastructure for the OpenTrapCamera Project"""
 
 import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+bucket = s3.BucketV2('image-bucket')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
