2140ee858b605f0cd887c2827a7bc29b16d275f3
Joe Gadd <joe@computer.local>
2025-02-10 11:12:54 +0000

    basic infrastructure that will need a tweak

--- cloud_infra/__main__.py
+++ cloud_infra/__main__.py
@@ -2,9 +2,9 @@
 
 import pulumi
 from pulumi_aws import s3
+from pulumi_aws import iam
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.BucketV2('image-bucket')
-
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
