959fd2acd66410a0b8c9f8163a509ed45d4fa651
themanontv <39599137+themanontv@users.noreply.github.com>
2025-02-10 14:53:54 +0000

    created initial installation script (#7)
    Co-authored-by: Joe Gadd <joe@computer.local>

--- camera/main.py
+++ camera/main.py
@@ -6,8 +6,8 @@ cwd = os.getcwd()
 file = capture.photo(cwd)
 
 # Get the config
-aws_access_key, aws_secret_key, bucket = config.ingest(cwd + "/config")
+aws_access_key, aws_secret_key, bucket_name = config.ingest(cwd + "/config")
 
 # Upload file
 session = aws.create_session(aws_access_key, aws_secret_key)
-aws.upload(session, file, bucket)
+aws.upload(session, file, bucket_name)
